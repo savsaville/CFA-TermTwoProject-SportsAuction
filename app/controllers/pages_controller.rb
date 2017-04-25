56c2eeb16551ce313b88c9fff241d242f9f8a934
Richard Saville <richardasaville@gmail.com>
2017-04-25 15:44:23 +1000

    added admin dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,13 @@
 class PagesController < ApplicationController
+  before_filter :authorize_admin, only: :admin_dash
+
+
   def home
+    @items = Item.all
+  end
+
+  def admin_dash
+    @items = Item.all
+    @users = User.all
   end
 end
